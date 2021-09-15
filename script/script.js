24007f584574686cd868a971ef553f6d5a826a1c
Erinaldo Prado <60299109+eri-prado@users.noreply.github.com>
2021-09-14 21:34:54 -0300

    commit changes

--- script/script.js
+++ script/script.js
@@ -6,22 +6,26 @@ checkbox.addEventListener("change", function () {
   html.classList.toggle("dark-mod");
 });
 
-
 // Script
-const primeiraNota = Number(document.querySelector('#nota1'))
-const segundaNota = Number(document.querySelector('#nota2'))
-const terceiraNota = Number(document.querySelector('#nota3'))
-const quartaNota = Number(document.querySelector('#nota4'))
+const calcular = document.querySelector(".calcular");
+const resultado = document.querySelector(".resultado");
 
-const resultadoFinal = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4
+function calculo() {
+  const nota1 = Number(document.querySelector("#nota1").value);
+  const nota2 = Number(document.querySelector("#nota2").value);
+  const nota3 = Number(document.querySelector("#nota3").value);
+  const nota4 = Number(document.querySelector("#nota4").value);
+  const resultadoFinal = Number((nota1 + nota2 + nota3 + nota4) / 4);
 
-resultadoFinal.addEventListener('onclick', function resultado() {
-  return 'oi'
-})
+  if (nota1 && nota2 && nota3 && nota4 !== "") {
+    if (resultadoFinal >= 7 && resultadoFinal <= 10) {
+      resultado.textContent = `Você foi APROVADO! Sua nota final é ${resultadoFinal.toFixed(2)}.`;
+    } else {
+      resultado.textContent = `Você foi REPROVADO! Sua nota final é ${resultadoFinal.toFixed(2)}.`;
+    }
+  } else {
+    resultado.textContent = "Preencha todos os valores!";
+  }
+}
 
-
-// if (notaFinal >= 7) {
-//   console.log('Você foi APROVADO! Sua nota é ' + notaFinal.toFixed(2)) 
-// } else {
-//   console.log('Você foi REPROVADO! Infelizmente sua nota é ' + notaFinal.toFixed(2))
-//   }
+calcular.addEventListener("click", calculo);
